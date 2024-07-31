f90d030eea6611a54a49e5febdca7c88c79aeb36
Markus Lång <markus.lang@aalto.fi>
2024-07-31 17:13:40 +0300

    Chunk stores Voxel pointers in vector

--- src/world/chunk/chunk.cpp
+++ src/world/chunk/chunk.cpp
@@ -10,27 +10,22 @@ Chunk::Chunk(int width, int height, int depth, glm::vec2 index)
 }
 
 void Chunk::generateTerrain() {
-
-    // Reserve space for all chunk voxels
-    voxels.reserve(width * height * depth);
-    
-
+    voxels.resize(width * height * depth, nullptr);
     for (int x = 0; x < width; ++x) {
         for (int z = 0; z < depth; ++z) {
-            int y = generateHeight(x, z);
-            for (int h = 0; h < y; ++h) {
-                glm::vec3 voxelPosition(x, h, z);
-                if (h < y / 2) {  
-                    voxels.emplace_back(voxelPosition, STONE);
+            int maxY = generateHeight(x, z);
+            for (int y = 0; y < maxY; ++y) {
+                glm::vec3 voxelPosition(x, y, z);
+                unsigned int voxelIndex = coordsToIndex(voxelPosition);
+                if (y < maxY / 2) {
+                    voxels[voxelIndex] = new Voxel(voxelPosition, STONE);
                 } else {
-                    voxels.emplace_back(voxelPosition, GRASS);
+                    voxels[voxelIndex] = new Voxel(voxelPosition, GRASS);
                 }
             }
         }
     }
 }
-
-
 
 int Chunk::generateHeight(int x, int z) const {
     float scale = 0.02f; 
@@ -41,11 +36,13 @@ int Chunk::generateHeight(int x, int z) const {
 
 std::vector<float> Chunk::getVertexData() const {
     std::vector<float> vertices;
+    // ofsett based on chunk location:
     glm::vec3 offset(index_.x * width, 0, index_.y * depth);
-
-    for (const auto& voxel : voxels) {
-        std::vector<float> voxelVertices = voxel.getVertexData(offset);
-        vertices.insert(vertices.end(), voxelVertices.begin(), voxelVertices.end());
+    for (const Voxel* voxel : voxels) {
+        if (voxel != nullptr) {
+            std::vector<float> voxelVertices = voxel->getVertexData(offset);
+            vertices.insert(vertices.end(), voxelVertices.begin(), voxelVertices.end());
+        }
     }
     return vertices;
 }
@@ -53,37 +50,53 @@ std::vector<float> Chunk::getVertexData() const {
 std::vector<unsigned int> Chunk::getIndexData() const {
     std::vector<unsigned int> indices;
     unsigned int baseIndex = 0;
-
-    for (const auto& voxel : voxels) {
-        std::vector<unsigned int> voxelIndices = voxel.getIndexData(baseIndex);
-        indices.insert(indices.end(), voxelIndices.begin(), voxelIndices.end());
-        baseIndex += 24;
+    for (const Voxel* voxel : voxels) {
+        if (voxel != nullptr) {
+            std::vector<unsigned int> voxelIndices = voxel->getIndexData(baseIndex);
+            indices.insert(indices.end(), voxelIndices.begin(), voxelIndices.end());
+            baseIndex += 24; // Number of vertices per voxel
+        }
     }
     return indices;
 }
 
+
+Voxel* Chunk::getVoxel(const glm::vec3& pos) const {
+    unsigned int index = coordsToIndex(pos);
+    if (index < voxels.size()) {
+        return voxels[index];
+    }
+    return nullptr;
+}
+
+void Chunk::setVoxel(const glm::vec3& pos, VoxelType type) {
+    unsigned int index = coordsToIndex(pos);
+    if (index < voxels.size()) {
+        delete voxels[index];
+        voxels[index] = new Voxel(pos, type);
+    }
+}
+
 std::vector<float> Chunk::getMesh() const {
-    // Assuming this function should return a combination of vertex and index data
     std::vector<float> mesh = getVertexData();
     std::vector<unsigned int> indices = getIndexData();
     mesh.insert(mesh.end(), indices.begin(), indices.end());
     return mesh;
 }
 
-
-glm::vec3 Chunk::indexToCoords(int index) const {
-    return glm::vec3(
-        static_cast<float>(index % 16),
-        static_cast<float>(index / (16 * 16)),
-        static_cast<float>((index / 16) % 16)
+unsigned int Chunk::coordsToIndex(glm::vec3 coords) const {
+    return static_cast<unsigned int>(
+        coords.x + 
+        coords.y * width + 
+        coords.z * width * height
     );
 }
 
-unsigned int Chunk::coordsToIndex(const glm::vec3 coords) const {
-    return static_cast<int>(
-        coords.x + 
-        coords.y * 16 * 16 + 
-        coords.z * 16
+glm::vec3 Chunk::indexToCoords(int index) const {
+    return glm::vec3(
+        index % width,
+        (index / width) % height,
+        index / (width * height)
     );
 }
 
--- src/world/chunk/chunk.h
+++ src/world/chunk/chunk.h
@@ -9,30 +9,32 @@
 
 class Chunk {
 public:
+
     Chunk(int width, int height, int depth, glm::vec2 index);
 
     std::vector<float> getVertexData() const;
     std::vector<unsigned int> getIndexData() const;
     std::vector<float> getMesh() const; // Move to chunkmesh
-    Voxel getPosTile(glm::vec3 pos) const;
-    
+    Voxel* getVoxel(const glm::vec3& pos) const;
+    void setVoxel(const glm::vec3& pos, VoxelType type);
+
     glm::vec2 getIndex() const;
     bool operator==(const Chunk& other) const;
 
 
+private:
+
+    // Convert block coordinates to array index and back
     glm::vec3 indexToCoords(int index) const;
     unsigned int coordsToIndex(glm::vec3 coords) const;
 
-
-
-private:
     void generateTerrain();
     int generateHeight(int x, int z) const;
 
 
     int width, height, depth;
     glm::vec2 index_;
-    std::vector<Voxel> voxels;
+    std::vector<Voxel*> voxels;
     Perlin perlinNoise; 
 };
 
