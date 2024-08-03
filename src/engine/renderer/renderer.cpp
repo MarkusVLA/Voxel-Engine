0ca096e570f6b133eced2ab2e509b2e107807304
Markus Lång <markus.lang@aalto.fi>
2024-08-03 18:45:28 +0300

    pls

--- src/engine/renderer/renderer.cpp
+++ src/engine/renderer/renderer.cpp
@@ -39,6 +39,7 @@ Renderer::~Renderer() {
 
 }
 
+
 void Renderer::initOpenGL() {
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
@@ -223,11 +224,12 @@ void Renderer::loadTexture(const std::string& path) {
 }
 
 void Renderer::draw() {
+
     if (!textureLoaded || !camera) {
         return;
     }
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
     glm::mat4 view = camera->getViewMatrix();
 
